b64a7fdcdc28e1f033068b2699da28c7d0cacc0d
wood <gelinw3@gmail.com>
2022-10-03 17:18:49 -0400

    fix distorted image

--- src/Section 3 Component/Project.js
+++ src/Section 3 Component/Project.js
@@ -1,7 +1,8 @@
 import "./Project.css"
 function Project ({name, link, image}){
     return(
-        <div className= "project" style={{backgroundImage:`url(${image})`,  backgroundRepeat: 'no-repeat', backgroundSize: '100% 100%', height: '50vh'}}>
+        <div className= "project" style={{backgroundImage:`url(${image})`, 
+        backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
             <div className ="dark">
                 <section id="slideText">
                     <h2>{name}</h2>
