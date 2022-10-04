a638c2bb4d1ac37a264ff8ad3763579576e67b49
wood <gelinw3@gmail.com>
2022-10-03 20:00:38 -0400

    center contact info

--- src/Section 4 Component/MyInfo.js
+++ src/Section 4 Component/MyInfo.js
@@ -5,15 +5,15 @@ import pin from "../Icons/pin.svg"
 function MyInfo(){
     return(
         <div id="MyInfoCOntainer">
-            <div className="address">
-            <img className="contactPic" src={pin} alt = "pin icon"></img>
-                <p id="span1"><b>WHERE TO FIND ME</b></p>
-                <p id="p1">Brooklyn, New York 11210</p>
-            </div>
             <div className="mail">
             <img className="contactPic" src={mail} alt = "mail icon"></img>
                 <p  id="span1"><b>EMAIL ME AT</b></p>
                 <p id="p1"><a id="p1" href = "mailto: gelinw3@gmail.com">gelinw3@gmail.com</a></p>
+            </div>
+            <div className="address">
+            <img className="contactPic" src={pin} alt = "pin icon"></img>
+                <p id="span1"><b>WHERE TO FIND ME</b></p>
+                <p id="p1">Brooklyn, New York 11210</p>
             </div>
             <div className="telephone">
             <img className="contactPic" src={phone} alt = "telephone icon"></img>
