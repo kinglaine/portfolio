8dc9c87878b59c90b8bab10d28d4c104411663b1
wood <gelinw3@gmail.com>
2022-10-03 20:06:53 -0400

    add new resume

--- src/Section 2 Component/DownloadCV.js
+++ src/Section 2 Component/DownloadCV.js
@@ -1,7 +1,7 @@
 import "./DownloadCV.css"
 function DownloadCV(){
     return(
-        <a href="https://drive.google.com/file/d/19gEhOZmQBzkWRsI3WWw5atLSNntuFJD2/view?usp=sharing" target="_blank">
+        <a href="https://drive.google.com/file/d/1XeHJYAi737nCaBstAzkT21J0JHOkhTFR/view?usp=sharing" target="_blank">
             <button className="Download">
                 <span><b>RESUME</b></span>
             </button>
