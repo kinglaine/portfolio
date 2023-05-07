25f718551b1b420a5cd29a22ba5f71ef361efb64
woodley <gelinw3@gmail.com>
2023-05-07 17:34:28 -0400

    fix wrong github link

--- src/Section 1 Component/Connect.js
+++ src/Section 1 Component/Connect.js
@@ -6,7 +6,7 @@ function Connect(){
     return(
         <div className="connect">
             <a href="https://www.linkedin.com/in/woodley-gelin-0bb9211a1/" target="_blank" rel=" noopener noreferrer"><img id= "linkedin" src = {linkedIn} alt = "linkedIn logo"></img></a>
-            <a href="https://github.com/kinglaine/Kinglaine/blob/main/README.md" target="_blank" rel=" noopener noreferrer"><img id="github" src = {gitHub} alt = "gitHub logo"></img></a>
+            <a href="https://github.com/kinglaine" target="_blank" rel=" noopener noreferrer"><img id="github" src = {gitHub} alt = "gitHub logo"></img></a>
         </div>
     );
 }
