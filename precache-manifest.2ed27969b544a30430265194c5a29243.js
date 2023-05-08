9882ea45d9b228c26e67e1d3da309d576cfbfd08
woodley <gelinw3@gmail.com>
2023-05-07 21:36:46 -0400

    Updates

---
+++ precache-manifest.2ed27969b544a30430265194c5a29243.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "52505d58da4d1841689d",
+    "url": "/static/css/main.93e7cf8c.chunk.css"
+  },
+  {
+    "revision": "52505d58da4d1841689d",
+    "url": "/static/js/main.52505d58.chunk.js"
+  },
+  {
+    "revision": "94a23f00553421116fe0",
+    "url": "/static/js/1.94a23f00.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "05d8b3ff9e4dc4fbc0156c53d6222932",
+    "url": "/static/media/linkedin.05d8b3ff.svg"
+  },
+  {
+    "revision": "66577d23ce7eea715bad637b1ccefc6c",
+    "url": "/static/media/github.66577d23.svg"
+  },
+  {
+    "revision": "03bf81582ec0eb972652cbfd023359af",
+    "url": "/static/media/bottom.03bf8158.png"
+  },
+  {
+    "revision": "73b39cd951032c12a38e96debdad3c7c",
+    "url": "/static/media/Play.73b39cd9.png"
+  },
+  {
+    "revision": "01d54365ded1291e37eedabfd073ecbd",
+    "url": "/static/media/FlashCard.01d54365.jpg"
+  },
+  {
+    "revision": "6e18aa63bae490b1b36cdb3b23a6f77b",
+    "url": "/static/media/Exclusive.6e18aa63.png"
+  },
+  {
+    "revision": "b0cdef0620df88593499eef03343ddc2",
+    "url": "/static/media/phone.b0cdef06.svg"
+  },
+  {
+    "revision": "3630dba628119669a22983ec70090de0",
+    "url": "/static/media/mail.3630dba6.svg"
+  },
+  {
+    "revision": "d73ba147996babb3aef7a1a95ce08ec0",
+    "url": "/static/media/pin.d73ba147.svg"
+  },
+  {
+    "revision": "2e8e77dbdfbf7b1846e667f1187639e4",
+    "url": "/static/media/banner.2e8e77db.jpg"
+  },
+  {
+    "revision": "6c7c1703ba3d32a4d676c43f1fcc9473",
+    "url": "/index.html"
+  }
+];
