dc39a157ffa23d5a5de6a9865e6e7b03b6ac1635
woodley <gelinw3@gmail.com>
2023-10-10 16:56:50 -0400

    refactor: display two columns and add new techlologies

--- src/Section 2 Component/Technologies.js
+++ src/Section 2 Component/Technologies.js
@@ -7,38 +7,39 @@ function Technologies(){
             <span id="title"><b>TECHNOLOGIES</b></span>
             <p id="mains">The following tools represent my core competencies, but I'm always exploring and expanding my skillset.</p>
             <ul className="TechnologyList">
-                    <li className="techLogo">
-                        <span id="name">Android-Studio &nbsp;</span>
-                    </li>
-                    <p>Used to build 3 apps. Currently making an E-Commerce app.</p>
+                <li className="techLogo">
+                    <span id="name">Adobe XD &nbsp;</span>
+                </li>
+                {/* <p>Use it for designing and prototyping softwares.</p> */}
 
-                    <li className="techLogo">
-                    <span id="name">PostgreSQL &nbsp;</span>
-                    </li>
-                    <p>I use postgresql mostly for school projects.</p>
+                <li className="techLogo">
+                    <span id="name">Android-Studio &nbsp;</span>
+                </li>
+                {/* <p>Used to build 3 apps. Currently making an E-Commerce app.</p> */}
 
-                    <li className="techLogo">
-                        <span id="name">REACT &nbsp;</span> 
-                    </li>
-                    <p>This portfolio was built with React Js, and I have experience with React Native</p>
-                
-                    <li className="techLogo">
-                        <span id="name">Adobe XD &nbsp;</span>
-                    </li>
-                    <p>Use it for designing and prototyping softwares.</p>
-                
-                
-                    <li className="techLogo">
-                    <span id="name"> Eclipse &nbsp;</span>
-                    </li>
-                    <p>I use eclipse for class activities and mostly debugging.</p>
-       
-                    <li className="techLogo">
+                <li className="techLogo">
                     <span id="name">Git/Github &nbsp;</span>
-                    </li>
-                    <p>I use git and github to track and manage my projects.</p>
-                
+                </li>
+                {/* <p>I use git and github to track and manage my projects.</p> */}
+
+                <li className="techLogo">
+                    <span id="name">PostgreSQL &nbsp;</span>
+                </li>
+                {/* <p>I use postgresql mostly for school projects.</p> */}
+
+                <li className="techLogo">
+                    <span id="name">React &nbsp;</span> 
+                </li>
+                {/* <p>This portfolio was built with React Js, and I have experience with React Native</p> */}
+                <li className="techLogo">
+                    <span id="name">Redux &nbsp;</span> 
+                </li>
+                <li className="techLogo">
+                    <span id="name">Socket.io &nbsp;</span> 
+                </li>
             </ul>
+
+
 
         </div>
     );
