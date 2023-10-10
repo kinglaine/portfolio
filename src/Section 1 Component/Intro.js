6e0cf5137bb66ab2d60b33299fad27fe20b90978
woodley <gelinw3@gmail.com>
2023-10-10 17:48:01 -0400

    feat: create type writer animation in intro

--- src/Section 1 Component/Intro.js
+++ src/Section 1 Component/Intro.js
@@ -2,13 +2,14 @@ import "./Intro.css";
 import About from "./More Component/About";
 import Connect from "./Connect";
 import React from 'react';
+import Typewriter from "./More Component/Typewriter";
 function Header(){
     return(
         <section id="section1" className="section1">
             <div className="Info">
                 <h5 id="p1">Hello,</h5>
                 <h1 id="p2">I'm Woodley Gelin.</h1>
-                <h6 id="p3">| FRONT-END DEVELOPER | ANDROID DEVELOPER |</h6>
+                <Typewriter text={"| FRONT-END DEVELOPER | ANDROID DEVELOPER |"} delay={100} id={"p3"}></Typewriter>
                 <About></About>
                 <Connect></Connect>
             </div>
